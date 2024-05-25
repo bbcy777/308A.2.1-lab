d14086d2ecb2d019d0350517e5047e86aa3a04ae
bbcy777 <bbcy777@gmail.com>
2024-05-24 22:59:59 -0700

    first commit

---
+++ script.mjs
@@ -0,0 +1,9 @@
+//Part 1: Humble Beginnings
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+}
+adventurer.inventory.forEach(inventory => {
+    console.log(inventory);
+})
