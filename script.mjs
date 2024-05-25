ab3a004f8c29773e5bc661894347401caa0a2e75
bbcy777 <bbcy777@gmail.com>
2024-05-24 23:32:12 -0700

    part 2

--- script.mjs
+++ script.mjs
@@ -1,9 +1,51 @@
-//Part 1: Humble Beginnings
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"]
+// //Part 1: Humble Beginnings
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat"
+//     }
+// }
+// adventurer.inventory.forEach(inventory => {
+//     console.log(inventory);
+// })
+
+// adventurer.companion.companion = {
+//     name: 'Frank',
+//     type: 'Flea',
+//     belongings: ['hat', 'sunglasses']
+// }
+
+// adventurer.roll = function(mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+// };
+    
+// adventurer.roll();
+
+// Part 2: Class Fantasy
+class Character{
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+    }
 }
-adventurer.inventory.forEach(inventory => {
-    console.log(inventory);
-})
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll()
+
+//Part 3: Class Features
